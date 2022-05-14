8cc7452654bd2654ea011fac52c2f15cbdd0f6b7
Rome00 <r.khazalia@gmail.com>
2022-05-14 16:13:03 +0400

    🚚 add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,17 +2,27 @@ import type { RouteRecordRaw } from 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'Home',
-        component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
-    },
+  {
+    path: '/',
+    name: 'Movie',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+  },
+  {
+    path: '/tv',
+    name: 'TV',
+    component: () => import(/* webpackChunkName: "home" */ '../views/TvSeries.vue'),
+  },
+  {
+    path: '/media/:id',
+    name: 'Media',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Media.vue'),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
-    linkActiveClass: 'active',
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkActiveClass: 'active',
 });
 
 export default router;
